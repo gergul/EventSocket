c156a51a15450857532e4808937f59fceeb6d6a7
Gergul <mail0668@gmail.com>
2019-02-16 14:49:09 +0800

    更新注释

--- EventSocket/TCP_Client_Sync.h
+++ EventSocket/TCP_Client_Sync.h
@@ -9,6 +9,13 @@ public:
 	TCP_Client_Sync();
 	virtual ~TCP_Client_Sync();
 	
+	///发送数据，并等待返回
+	//  dataRet - 返回的数据
+	//  datalenRet - 返回的数据的长度
+	//  waitRet - 等待的结果。0:正常；1:超时；-1:出错
+	//  timeout - 超时时间，毫秒。为0时无限等待
+	//  data - 要发送的数据
+	//  datalen - 要发送的数据长度
 	virtual int sendAndWait(const char*& dataRet, int& datalenRet, int& waitRet, long timeout, const char* data, int datalen = 0);
 
 protected:
--- EventSocket/TCP_Session_SyncEx.h
+++ EventSocket/TCP_Session_SyncEx.h
@@ -11,7 +11,11 @@ public:
 	virtual int send(const char* data, int datalen = 0) override;
 
 protected:
+	///获得数据的事件
+	//  获得的是带标识的原始数据
 	virtual void onRecv(const char* data, int dataLen) override;
+	///获得数据的事件
+	//  获得的是不带标识的原始数据
 	virtual void onRecvEx(const char* data, int dataLen);
 
 protected:
